f54245421c49b8410c958eff80ce0fa3b8da791c
Bethusile <bethusilejm@gmail.com>
2025-08-19 03:54:32 +0200

    added admin to restrict its functions

--- Project bet/Project/Controllers/ProfileController.cs
+++ Project bet/Project/Controllers/ProfileController.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using Project.Data;
 using Project.Models;
@@ -7,6 +8,7 @@ using System.Threading.Tasks;
 
 namespace Project.Controllers
 {
+    
     public class ProfileController : Controller
     {
         private readonly ApplicationDbContext _context;
---
+++ Project bet/Project/Data/DbInitializer.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.Extensions.DependencyInjection;
+
+public static class DbInitializer
+{
+    public static async Task SeedRolesAndAdminAsync(IServiceProvider serviceProvider)
+    {
+        var roleManager = serviceProvider.GetRequiredService<RoleManager<IdentityRole>>();
+        var userManager = serviceProvider.GetRequiredService<UserManager<IdentityUser>>();
+
+        string[] roleNames = { "Admin", "Client" };
+        foreach (var roleName in roleNames)
+        {
+            if (!await roleManager.RoleExistsAsync(roleName))
+            {
+                await roleManager.CreateAsync(new IdentityRole(roleName));
+            }
+        }
+
+        // Create a default admin user if one doesn't exist
+        var adminUser = new IdentityUser { UserName = "admin@test.com", Email = "admin@test.com", EmailConfirmed = true };
+        if (await userManager.FindByEmailAsync(adminUser.Email) == null)
+        {
+            await userManager.CreateAsync(adminUser, "AdminPassword123!");
+            await userManager.AddToRoleAsync(adminUser, "Admin");
+        }
+    }
+}
